a30e86b3157947a68ff1c8a5ddc0cf8750aebd23
netebakari <itsumo.itsumo.nemui@gmail.com>
2020-02-03 11:16:53 +0900

    1行を120文字にした

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as LambdaTypes from "aws-lambda";
 import * as env from "./env";
 
-exports.handler = async (
-  event: LambdaTypes.APIGatewayEvent | any,
-  context: LambdaTypes.Context
-) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+exports.handler = async (event: LambdaTypes.APIGatewayEvent | any, context: LambdaTypes.Context): Promise<boolean> => {
   console.log("HELLO WORLD");
   return true;
 };
--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,10 @@
 import * as assert from "assert";
-import * as myModule from "../src/index"
+import * as myModule from "../src/index";
 import * as mocha from "mocha";
 
 describe("myModule", () => {
   describe("test", () => {
-    before(async() => {
-
-    });
+    before(async () => {});
 
     it("test1", () => {
       assert.equal(1, 1);
